fc4ffc77022717dd1626ba72209ae041379e3054
NoistNT <arielgnr23@gmail.com>
2023-05-08 21:28:46 -0300

    Add and sync database. Add models (Activity, Country)

---
+++ api/src/controllers/getAPIData.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const { Country } = require('../db.js')
+const { URL } = process.env
+const axios = require('axios')
+
+// Get data from external API
+const getAPIData = async () => {
+  try {
+    const { data } = await axios.get(`${URL}/all`)
+
+    const countries = data.map((country) => {
+      return {
+        id: country.cca3,
+        name: country.name.common,
+        flag: country.flags.svg,
+        continent: country.continents[0],
+        capital: country.capital !== undefined ? country.capital[0] : 'No data',
+        subregion: country.subregion,
+        area: country.area,
+        population: country.population
+      }
+    })
+
+    return countries
+  } catch (error) {
+    console.error(error)
+    return error
+  }
+}
+
+// Load the retrieved data into the Country table
+const loadCountries = async () => {
+  try {
+    const countries = await Country.findAll()
+    if (!countries.length) {
+      const countriesLoaded = await getAPIData()
+      await Country.bulkCreate(countriesLoaded)
+      console.log('Database loaded successfully!')
+    }
+  } catch (error) {
+    console.error(`Error loading countries into Database: ${error}`)
+    return error
+  }
+}
+
+// Synchronize countries to database
+const syncCountriesToDB = async () => await loadCountries()
+
+syncCountriesToDB()
+
+module.exports = getAPIData
---
+++ api/src/models/Acivity.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    'Activity',
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      difficulty: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      duration: {
+        type: DataTypes.INTEGER
+      },
+      season: {
+        type: DataTypes.ENUM('Summer', 'Autumn', 'Winter', 'Spring'),
+        allowNull: false
+      }
+    },
+    { timestamps: false }
+  )
+}

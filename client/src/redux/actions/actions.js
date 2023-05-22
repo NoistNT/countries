6f5aeafa6306d62abd57f0da6fe63e3ce9f5a80a
NoistNT <arielgnr23@gmail.com>
2023-05-22 16:10:14 -0300

    Add types, actions and reducer cases

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -3,32 +3,58 @@ import {
   GET_COUNTRIES,
   GET_COUNTRY_BY_ID,
   GET_COUNTRIES_BY_NAME,
-  CLEAN_DETAIL
+  CREATE_ACTIVITY,
+  CLEAN_DETAIL,
+  FILTER_BY_CONTINENT,
+  SORT_COUNTRIES
 } from './types'
 
-const URL = 'http://localhost:3001/countries'
+const URL = 'http://localhost:3001'
 
 export const getCountries = () => {
   return async (dispatch) => {
-    const { data } = await axios.get(URL)
+    const { data } = await axios.get(`${URL}/countries`)
     dispatch({ type: GET_COUNTRIES, payload: data })
   }
 }
 
 export const getCountryById = (id) => {
   return async (dispatch) => {
-    const { data } = await axios.get(`${URL}/${id}`)
+    const { data } = await axios.get(`${URL}/countries/${id}`)
     dispatch({ type: GET_COUNTRY_BY_ID, payload: data })
   }
 }
 
 export const getCountriesByName = (name) => {
   return async (dispatch) => {
-    const { data } = await axios.get(`${URL}?name=${name}`)
-    dispatch({ type: GET_COUNTRIES_BY_NAME, payload: data })
+    try {
+      const { data } = await axios.get(`${URL}/countries?name=${name}`)
+      dispatch({ type: GET_COUNTRIES_BY_NAME, payload: data })
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+}
+
+export const createActivity = (activity) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post(`${URL}/activities`, activity)
+      dispatch({ type: CREATE_ACTIVITY, payload: data })
+    } catch (error) {
+      console.log(error.response.data)
+    }
   }
 }
 
 export const cleanDetail = () => {
   return { type: CLEAN_DETAIL }
+}
+
+export const filterByContinent = (continent) => {
+  return { type: FILTER_BY_CONTINENT, payload: continent }
+}
+
+export const getSorted = (data) => {
+  return { type: SORT_COUNTRIES, payload: data }
 }
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -2,12 +2,20 @@ import {
   GET_COUNTRIES,
   GET_COUNTRY_BY_ID,
   GET_COUNTRIES_BY_NAME,
-  CLEAN_DETAIL
+  CREATE_ACTIVITY,
+  CLEAN_DETAIL,
+  FILTER_BY_CONTINENT,
+  SORT_COUNTRIES
 } from './actions/types'
 
 const initialState = {
+  allCountries: [],
   countries: [],
-  countryDetail: {}
+  country: {},
+  activities: [],
+  isSorted: false,
+  filter: 'all',
+  sort: ''
 }
 
 export default function rootReducer(state = initialState, { type, payload }) {
@@ -15,22 +23,87 @@ export default function rootReducer(state = initialState, { type, payload }) {
     case GET_COUNTRIES:
       return {
         ...state,
-        countries: payload
+        allCountries: payload,
+        countries: payload,
+        isSorted: false
       }
     case GET_COUNTRY_BY_ID:
       return {
         ...state,
-        countryDetail: payload
+        country: payload
       }
     case GET_COUNTRIES_BY_NAME:
       return {
         ...state,
         countries: payload
       }
+    case CREATE_ACTIVITY:
+      return {
+        ...state,
+        activities: payload
+      }
     case CLEAN_DETAIL:
       return {
         ...state,
-        countryDetail: {}
+        country: {}
+      }
+    case FILTER_BY_CONTINENT:
+      const { allCountries, sort } = state
+      const filtered =
+        payload === 'all'
+          ? allCountries
+          : allCountries.filter((country) => country.continent === payload)
+
+      let sorted = filtered
+      if (sort === 'az')
+        sorted = [...filtered].sort((a, b) => a.name.localeCompare(b.name))
+      if (sort === 'za')
+        sorted = [...filtered].sort((a, b) => b.name.localeCompare(a.name))
+      if (sort === 'higher')
+        sorted = [...filtered].sort((a, b) => b.population - a.population)
+      if (sort === 'lower')
+        sorted = [...filtered].sort((a, b) => a.population - b.population)
+
+      return {
+        ...state,
+        countries: sorted,
+        filter: payload,
+        isSorted: true
+      }
+
+    case SORT_COUNTRIES:
+      const { countries, filter } = state
+      let sortedCountries
+      if (payload === 'az') {
+        sortedCountries = [...countries].sort((a, b) =>
+          a.name.localeCompare(b.name)
+        )
+      } else if (payload === 'za') {
+        sortedCountries = [...countries].sort((a, b) =>
+          b.name.localeCompare(a.name)
+        )
+      } else if (payload === 'higher') {
+        sortedCountries = [...countries].sort(
+          (a, b) => b.population - a.population
+        )
+      } else if (payload === 'lower') {
+        sortedCountries = [...countries].sort(
+          (a, b) => a.population - b.population
+        )
+      } else {
+        sortedCountries = countries
+      }
+
+      const filteredCountries =
+        filter === 'all'
+          ? sortedCountries
+          : sortedCountries.filter((country) => country.continent === filter)
+
+      return {
+        ...state,
+        countries: filteredCountries,
+        sort: payload,
+        isSorted: true
       }
     default:
       return { ...state }

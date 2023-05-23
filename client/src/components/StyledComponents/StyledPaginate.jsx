3fcaa161db9a53e9ff14f022356ed3ec45383554
NoistNT <arielgnr23@gmail.com>
2023-05-23 11:20:39 -0300

    Add Paginate component and styles

---
+++ client/src/components/StyledComponents/StyledPaginate.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const PaginateContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 1.5rem 0;
+`
+export const PageNumber = styled.li`
+  list-style-type: none;
+  padding: 0.3rem 0.9rem;
+  margin: 0 0.15rem;
+  background-color: #22223b;
+  color: #ffffffe0;
+  font-family: 'Raleway', sans-serif;
+  border-radius: 0.3rem;
+  transition: 0.05s;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: -1px 3px 28px -4px rgba(0, 0, 0, 0.74);
+    -webkit-box-shadow: -1px 3px 28px -4px rgba(0, 0, 0, 0.74);
+    -moz-box-shadow: -1px 3px 28px -4px rgba(0, 0, 0, 0.74);
+    transition: 0.05s;
+    font-weight: 600;
+  }
+
+  &:active,
+  &:focus {
+    color: #4941b3;
+  }
+`

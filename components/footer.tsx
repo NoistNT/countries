e87430ebdc313958b0d64cd7341261822cc81d2c
NoistNT <arielgnr23@gmail.com>
2024-04-19 13:54:23 -0300

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { CardDescription, CardFooter } from './ui/card'
 
 export default function Footer() {
@@ -5,12 +7,13 @@ export default function Footer() {
     <CardFooter className="flex justify-center py-6 leading-[3rem] text-muted-foreground opacity-80">
       <CardDescription>
         Made with ❤️ by{' '}
-        <a
+        <Link
           className="text-align-center text-sky-700 hover:underline dark:text-cyan-400"
           href="https://github.com/NoistNT"
+          target="_blank"
         >
           Ariel Piazzano
-        </a>
+        </Link>
       </CardDescription>
     </CardFooter>
   )

da3d78c0ecb55d75c9bd9204a9e100814dbe8508
NoistNT <arielgnr23@gmail.com>
2024-05-11 15:02:06 -0300

    Add API.ts

---
+++ client/api.ts
@@ -0,0 +1,31 @@
+import type { ICountry, ICountrySimple } from '@/types'
+
+const { API_URL } = process.env
+
+export async function getCountries() {
+  try {
+    const response = await fetch(`${API_URL!}/countries`)
+
+    if (!response.ok) throw new Error('Failed to fetch countries')
+
+    return ((await response.json()) as ICountrySimple[]) ?? []
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message)
+
+    return []
+  }
+}
+
+export async function getCountry(id: string) {
+  try {
+    const response = await fetch(`${API_URL!}/countries/${id}`)
+
+    if (!response.ok) throw new Error(`Failed to fetch country: ${id}`)
+
+    return (await response.json()) as ICountry
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message)
+
+    return null
+  }
+}

da94c24aa7441449c090287e7e3cbe68e88762d2
NoistNT <arielgnr23@gmail.com>
2024-05-12 22:23:13 -0300

    Update error component

--- client/app/error.tsx
+++ client/app/error.tsx
@@ -4,4 +4,6 @@ export default function ErrorPage({ error }: { error: Error }) {
   if (error instanceof Error) {
     return <div>{error.message}</div>
   }
+
+  return <div>Something went wrong</div>
 }

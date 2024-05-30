94e878472d2ee4cc4145d9a27607007fbef240a4
NoistNT <arielgnr23@gmail.com>
2024-05-29 23:21:55 -0300

    Minor update in main page component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,9 @@
+import type { ICountrySimple } from '@/types'
+
 import CountryCards from '@/components/country/country-cards'
-import { ICountrySimple } from '@/types'
 
 export default async function Home() {
-  const countries = await fetch(`${process.env.API_URL}/countries`)
+  const countries = await fetch(`${process.env.API_URL!}/countries`)
     .then((res) => res.json())
     .then((data) => data as ICountrySimple[])
 

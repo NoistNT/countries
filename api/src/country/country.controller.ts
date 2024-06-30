ff5fa7a76d43f6ddeb24c2463f32521db7409f2a
NoistNT <arielgnr23@gmail.com>
2024-06-29 23:54:03 -0300

    Fix lint in country controller

--- api/src/country/country.controller.ts
+++ api/src/country/country.controller.ts
@@ -10,6 +10,7 @@ export class CountryController {
   async findAll(@Body() filter?: CountryFilterDto) {
     return await this.countryService.findAll(filter);
   }
+
   @Get(':id')
   async findById(@Param('id') id: string) {
     return await this.countryService.findById(id);

c1c67fb98f8050c2bbc42570a497c0d39837ee04
NoistNT <arielgnr23@gmail.com>
2024-11-07 11:00:16 -0300

    Update next js to v15

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,12 +1,9 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     remotePatterns: [{ hostname: 'https://flagcdn.com/' }],
     loader: 'default'
   }
-}
+};
 
-export default nextConfig
+export default nextConfig;
